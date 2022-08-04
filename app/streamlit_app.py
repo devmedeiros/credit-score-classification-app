b414e21342c47ff9b6e1e2e835a6d88259ca4c01
Jaqueline Medeiros <jaqueline@devmedeiros.com>
2022-08-04 12:54:52 -0300

    incremented streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,15 +4,8 @@ import pandas as pd
 from zipfile import ZipFile
 import os
 from src import transform_resp
-
-path = os.path.dirname(__file__)
-zip_path = os.path.join(path,'../models/model.zip')
-folder_path = os.path.join(path,'../models')
-
-ZipFile(zip_path).extractall(folder_path)
-
-scaler = pickle.load(open(os.path.join(path,'../models/scaler.obj'), 'rb'))
-model = pickle.load(open(os.path.join(path,'../models/model.obj'), 'rb'))
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title='Credit Score App', page_icon='💰', layout='wide',
                    initial_sidebar_state='auto', menu_items={
@@ -26,6 +19,19 @@ st.set_page_config(page_title='Credit Score App', page_icon='💰', layout='wide
                         If you are interested in Data Science you can see follow my work through my blog [devmedeiros.com](https://devmedeiros.com) or on LinkedIn [medeiros-jaqueline](https://www.linkedin.com/in/medeiros-jaqueline/).
                         '''
      })
+
+path = os.path.dirname(__file__)
+folder_path = os.path.join(path,'../models')
+
+@st.cache(allow_output_mutation=True)
+def unzip_load(name):
+    path_zip = os.path.join(path,'../models/'+name+'.zip')
+    ZipFile(path_zip).extractall(folder_path)
+    path_obj = os.path.join(path,'../models/'+name+'.obj')
+    return  pickle.load(open(path_obj, 'rb'))
+
+scaler = unzip_load('scaler')
+model = unzip_load('model')
 
 st.title('Credit Score Analysis')
 st.caption('Made by Jaqueline Medeiros')
@@ -57,35 +63,71 @@ with st.sidebar:
 
 st.header('Credit Score Results')
 
-placeholder = st.empty()
+col1, col2 = st.columns([3, 2])
 
-if button:
-    resp = {
-        'age': age,
-        'annual_income': annual_income,
-        'accounts': accounts,
-        'credit_cards': credit_cards,
-        'delayed_payments': delayed_payments,
-        'credit_card_ratio': credit_card_ratio,
-        'emi_monthly': emi_monthly,
-        'credit_history': credit_history,
-        'loans': loans,
-        'missed_payment': missed_payment,
-        'minimum_payment': minimum_payment
-    }
-    output = transform_resp(resp)
-    output = pd.DataFrame(output, index=[0])
-    output.loc[:,:] = scaler.transform(output)
+with col2:
+    x1 = [0, 6, 0]
+    x2 = [0, 4, 0]
+    x3 = [0, 2, 0]
+    y = ['0', '1', '2']
 
-    credit_score = model.predict(output)[0]
+    f, ax = plt.subplots(figsize=(5,2))
 
-    if credit_score == 1:
-        st.balloons()
-        placeholder.markdown('Your credit score is **GOOD**! Congratulations!')
-        st.markdown('According to our algorithm, a person with your payment behavior is likely to repay a loan.')
-    elif credit_score == 0:
-        placeholder.markdown('Your credit score is **REGULAR**.')
-        st.markdown('According to our algorithm, a person with your payment behavior is likely to repay some loans but still can miss payments.')
-    elif credit_score == -1:
-        placeholder.markdown('Your credit score is **POOR**. You need to work on your finances to improve your credit score.')
-        st.markdown('According to our algorithm, a person with your payment behavior is unlikely to repay a loan.')
+    p1 = sns.barplot(x=x1, y=y, color='#3EC300')
+    p1.set(xticklabels=[], yticklabels=[])
+    p1.tick_params(bottom=False, left=False)
+    p2 = sns.barplot(x=x2, y=y, color='#FAA300')
+    p2.set(xticklabels=[], yticklabels=[])
+    p2.tick_params(bottom=False, left=False)
+    p3 = sns.barplot(x=x3, y=y, color='#FF331F')
+    p3.set(xticklabels=[], yticklabels=[])
+    p3.tick_params(bottom=False, left=False)
+
+    plt.text(0.7, 1.05, "POOR", horizontalalignment='left', size='medium', color='white', weight='semibold')
+    plt.text(2.5, 1.05, "REGULAR", horizontalalignment='left', size='medium', color='white', weight='semibold')
+    plt.text(4.7, 1.05, "GOOD", horizontalalignment='left', size='medium', color='white', weight='semibold')
+
+    ax.set(xlim=(0, 6))
+    sns.despine(left=True, bottom=True)
+
+    figure = st.pyplot(f, width=5)
+
+with col1:
+
+    placeholder = st.empty()
+
+    if button:
+        resp = {
+            'age': age,
+            'annual_income': annual_income,
+            'accounts': accounts,
+            'credit_cards': credit_cards,
+            'delayed_payments': delayed_payments,
+            'credit_card_ratio': credit_card_ratio,
+            'emi_monthly': emi_monthly,
+            'credit_history': credit_history,
+            'loans': loans,
+            'missed_payment': missed_payment,
+            'minimum_payment': minimum_payment
+        }
+        output = transform_resp(resp)
+        output = pd.DataFrame(output, index=[0])
+        output.loc[:,:] = scaler.transform(output)
+
+        credit_score = model.predict(output)[0]
+
+        if credit_score == 1:
+            st.balloons()
+            t1 = plt.Polygon([[5, 0.5], [5.5, 0], [4.5, 0]], color='black')
+            placeholder.markdown('Your credit score is **GOOD**! Congratulations!')
+            st.markdown('According to our algorithm, a person with your payment behavior is likely to repay a loan.')
+        elif credit_score == 0:
+            t1 = plt.Polygon([[3, 0.5], [3.5, 0], [2.5, 0]], color='black')
+            placeholder.markdown('Your credit score is **REGULAR**.')
+            st.markdown('According to our algorithm, a person with your payment behavior is likely to repay some loans but still can miss payments.')
+        elif credit_score == -1:
+            t1 = plt.Polygon([[1, 0.5], [1.5, 0], [0.5, 0]], color='black')
+            placeholder.markdown('Your credit score is **POOR**. You need to work on your finances to improve your credit score.')
+            st.markdown('According to our algorithm, a person with your payment behavior is unlikely to repay a loan.')
+        plt.gca().add_patch(t1)
+        figure.pyplot(f)
